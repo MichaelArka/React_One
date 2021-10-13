bb1e3be5d05c111d171a1546ab8d34decda47678
Michal Andronik <michal.androni@gmail.com>
2021-10-13 14:13:34 +0200

    [15.3] Change 'Hero' Component from 'class component' to 'function component'

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,15 +1,11 @@
 import React from 'react';
 import styles from './Hero.scss';
 
-class Hero extends React.Component {
-  render() {
-    return (
+const Hero = () => (
       <header className={styles.component}>
         <h2 className={styles.title}>React Hero</h2>
         <img className={styles.image} src="/src/images/space.png"></img>
       </header>
-    )
-  }
-}
+    );
 
 export default Hero;
