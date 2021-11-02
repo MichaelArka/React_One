2771b824ce89f07906c4486ba6e36eb164b8394c
Michal Andronik <michal.androni@gmail.com>
2021-11-02 19:04:15 +0100

    Refactoring 'App' 'Column' 'List' and migration on Redux

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,18 +1,33 @@
 import React from 'react';
 import styles from './App.scss';
-import List from '../List/List.js';
-import {pageContents, listData} from '../../data/dataStore';
+import List from '../List/ListContainer';
+import {pageContents} from '../../data/dataStore';
+import PropTypes from 'prop-types';
 
 class App extends React.Component {
+  static propTypes = {
+    title: PropTypes.node,
+    subtitle: PropTypes.node,
+    list: PropTypes.array,
+  };
+
   render() {
+    const {lists} = this.props;
     return (
       <main className={styles.component}>
         <h1 className={styles.title}>{pageContents.title}</h1>
         <h2 className={styles.subtitle}>{pageContents.subtitle}</h2>
-        <List {...listData} />
+        {lists.map(listData => (
+          <List key={listData.id} {...listData} />
+        ))}
       </main>
     );
   }
 }
+
+App.propTypes = {
+  map: PropTypes.any,
+  lists: PropTypes.array,
+};
 
 export default App;
--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -3,14 +3,14 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 class Card extends React.Component {
-
   static propTypes = {
     title: PropTypes.string,
   };
 
   render() {
+    const {title} = this.props;
     return (
-      <section className={styles.component}><h3 className={styles.title}>{this.props.title}</h3>
+      <section className={styles.component}><h3 className={styles.title}>{title}</h3>
       </section>
     );
   }
