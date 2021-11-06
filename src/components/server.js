3a244d59d7178409690428104125cfb35618e6da
Michal Andronik <michal.androni@gmail.com>
2021-11-06 10:20:26 +0100

    test commit

--- src/components/server.js
+++ src/components/server.js
@@ -1,11 +1,9 @@
-/* global require, process */
-
 const jsonServer = require('json-server');
 const server = jsonServer.create();
 const router = jsonServer.router('dist/db/app.json');
 const middlewares = jsonServer.defaults({
   static: 'dist',
-  noCors: true
+  noCors: true,
 });
 const port = process.env.PORT || 3131;
 
