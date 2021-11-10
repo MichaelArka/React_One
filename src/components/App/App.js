5de4007d2883048bd09363c2527c926209a8d2fe
Michal Andronik <michal.androni@gmail.com>
2021-11-10 17:52:57 +0100

    [17.2] Install 'React Router' & add 'Home' component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,35 +1,10 @@
 import React from 'react';
-import styles from './App.scss';
-import List from '../List/ListContainer';
-import {pageContents} from '../../data/dataStore';
-import PropTypes from 'prop-types';
-import Search from '../Search/SearchContainer';
+import Home from '../Home/HomeContainer';
 
-class App extends React.Component {
-  static propTypes = {
-    title: PropTypes.node,
-    subtitle: PropTypes.node,
-    list: PropTypes.array,
-  };
-
-  render() {
-    const {lists} = this.props;
-    return (
-      <main className={styles.component}>
-        <h1 className={styles.title}>{pageContents.title}</h1>
-        <h2 className={styles.subtitle}>{pageContents.subtitle}</h2>
-        <Search />
-        {lists.map(listData => (
-          <List key={listData.id} {...listData} />
-        ))}
-      </main>
-    );
-  }
-}
-
-App.propTypes = {
-  map: PropTypes.any,
-  lists: PropTypes.array,
-};
+const App = () => (
+  <div>
+    <Home />
+  </div>
+);
 
 export default App;
